975e2d9bc675a84a0e52fab0dec4d57b6dd5de29
Mitch <mitchell.anderson@colorado.edu>
2018-08-18 15:13:18 -0600

    removed quotes on URL, prevented variable usage

--- s3backup.sh
+++ s3backup.sh
@@ -5,7 +5,7 @@ twilio_acct_sid=''
 auth_token=''
 tar --exclude='*.tar.bz' -cvjf /mnt/s3/$stamp.tar.bz /mnt
 aws s3 cp /mnt/s3/$stamp.tar.bz s3://$bucket/$stamp-backup
-curl 'https://api.twilio.com/2010-04-01/Accounts/$twilio_acct_sid/Messages.json' -X POST \
+curl https://api.twilio.com/2010-04-01/Accounts/$twilio_acct_sid/Messages.json -X POST \
 --data-urlencode 'To=' \
 --data-urlencode 'From=' \
 --data-urlencode 'Body=S3 backup complete' \
