e225ef13213f472dfff2b294187b75934431c111
Mitch <mitchell.anderson@colorado.edu>
2018-08-19 08:37:32 -0600

    added example for var message body

--- s3backup.sh
+++ s3backup.sh
@@ -3,10 +3,12 @@ stamp=$(date +%h-%d)
 bucket=''
 twilio_acct_sid=''
 auth_token=''
+#sms="Body=<Custom Alert Here>"
 tar --exclude='*.tar.bz' -cvjf /mnt/s3/$stamp.tar.bz /mnt
 aws s3 cp /mnt/s3/$stamp.tar.bz s3://$bucket/$stamp-backup
 curl https://api.twilio.com/2010-04-01/Accounts/$twilio_acct_sid/Messages.json -X POST \
 --data-urlencode 'To=' \
 --data-urlencode 'From=' \
 --data-urlencode 'Body=S3 backup complete' \
+#--data-urlencode "$sms" \
 -u $twilio_acct_sid:$auth_token
